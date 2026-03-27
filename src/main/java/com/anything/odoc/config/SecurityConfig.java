b211038fd4687855c62d24265b0ed3c4779d3b2b
jeongChiBong <hcd1228@gmail.com>
2026-03-27 13:38:46 +0900

    logout permit 추가

--- src/main/java/com/anything/odoc/config/SecurityConfig.java
+++ src/main/java/com/anything/odoc/config/SecurityConfig.java
@@ -39,7 +39,8 @@ public class SecurityConfig {
                                 "/updateProject",
                                 "/insertProject",
                                 "/commitProject",
-                                "/updateAlert"
+                                "/updateAlert",
+                                "/userLogout"
                         ).permitAll()
                         .anyRequest().authenticated()
                 )
