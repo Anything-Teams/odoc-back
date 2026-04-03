531409cffade4f7e935c16fc25d03592b5c35d96
jeongChiBong <hcd1228@gmail.com>
2026-04-03 14:26:26 +0900

    기본테마 삽입 삭제, free로 대체

--- src/main/java/com/anything/odoc/project/ProjectUserService.java
+++ src/main/java/com/anything/odoc/project/ProjectUserService.java
@@ -23,20 +23,7 @@ public class ProjectUserService {
 
     public int userRegister(ProjectUserVO projectUserVO) {
         projectUserVO.setUserPw(passwordEncoder.encode(projectUserVO.getUserPw()));
-        int result = projectMainDao.userRegister(projectUserVO);
-
-        if(result > 0) {
-            ProjectMainVO projectMainVO = new ProjectMainVO();
-            projectMainVO.setUserId(projectUserVO.getUserId());
-
-            List<Integer> defaultThemaIds = Arrays.asList(0, 1);
-
-            for (Integer themaId : defaultThemaIds) {
-                projectMainVO.setThemaId(themaId);
-                projectMainDao.insertUserThema(projectMainVO);
-            }
-        }
-        return result;
+        return projectMainDao.userRegister(projectUserVO);
     }
 
     public int userIdCheck(ProjectUserVO projectUserVO) {
