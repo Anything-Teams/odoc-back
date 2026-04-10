9b8f5d1508fb35b0ed52b2a97504c92ff59eb6a4
jeongChiBong <hcd1228@gmail.com>
2026-04-10 17:30:49 +0900

    오류수정

--- src/main/java/com/anything/odoc/push/PushScheduler.java
+++ src/main/java/com/anything/odoc/push/PushScheduler.java
@@ -7,6 +7,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 import java.time.LocalTime;
 import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
@@ -36,7 +38,8 @@ public class PushScheduler {
             try {
                 String title = "지금 한 번 체크해보세요!";
                 String body = "[" + target.getOdocNames() + "] 시간입니다";
-                String link = "https://odoc.vercel.app/push-open?odocSn=" + target.getOdocSn();
+                String link = "https://odoc.vercel.app/push-open?odocSn=" + target.getOdocSn()
+                    + "&odocNm=" + URLEncoder.encode(target.getOdocNames(), StandardCharsets.UTF_8);
 //                String link = "http://localhost:3000/";
 
                 String response = fcmSendService.sendToToken(
