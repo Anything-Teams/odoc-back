2ff5f84ca2a81e21489299b441b62882d52d38c9
anything <hcd1228@gmail.com>
2026-04-11 01:30:38 +0900

    안정화

--- src/main/java/com/anything/odoc/push/PushScheduler.java
+++ src/main/java/com/anything/odoc/push/PushScheduler.java
@@ -38,8 +38,7 @@ public class PushScheduler {
             try {
                 String title = "지금 한 번 체크해보세요!";
                 String body = "[" + target.getOdocNames() + "] 시간입니다";
-                String link = "https://odoc.vercel.app/?pushTarget=" + target.getOdocSn()
-                    + "&odocNm=" + URLEncoder.encode(target.getOdocNames(), StandardCharsets.UTF_8);
+                String link = "https://odoc.vercel.app";
 //                String link = "http://localhost:3000/";
 
                 String response = fcmSendService.sendToToken(
